6d41016f174333fbffe376aed7fc436c6aec25df
clygd <clygd@126.com>
2017-09-30 18:49:30 +0800

    api

--- app/Http/Controllers/Api/V1/UserController.php
+++ app/Http/Controllers/Api/V1/UserController.php
@@ -2,10 +2,20 @@
 
 namespace App\Http\Controllers\Api\V1;
 
+use App\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 class UserController extends Controller
 {
     //
+    public function index()
+    {
+        return User::all();
+    }
+
+    public function show($id)
+    {
+        return User::findOrFail($id);
+    }
 }
