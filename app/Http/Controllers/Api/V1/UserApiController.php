ab703a5162ccd8d12237db5f46296e4d2adad7b6
clyhs <clygd@126.com>
2017-10-01 10:18:35 +0800

    userapi

--- app/Http/Controllers/Api/V1/UserApiController.php
+++ app/Http/Controllers/Api/V1/UserApiController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Api\V1;
 
 use App\User;
 use Illuminate\Http\Request;
-use App\Http\Controllers\Transformers;
+use App\Http\Controllers\Transformers\UserTransformer;
 
 class UserApiController extends BaseController
 {
